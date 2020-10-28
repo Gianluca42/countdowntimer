d156dbbaa85d347509cbeb6b1fed0572d7348bb6
Gianluca42 <55921840+Gianluca42@users.noreply.github.com>
2020-10-27 21:09:27 -0300

    Add files via upload

---
+++ CountMessage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountMessage : MonoBehaviour
+{
+
+    public Text message;
+    public int CountdownTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message.text = "You have ten seconds to get all the flowers. Do your job!";
+
+        StartCoroutine(Countdown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    IEnumerator Countdown()
+    {
+
+       
+        while (CountdownTime > 0)
+        {
+
+            message.text = CountdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            CountdownTime--;
+
+        }
+        message.gameObject.SetActive(false);
+        
+
+   
+    }
+}
---
+++ CountdownControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownControllerScript : MonoBehaviour
+{
+
+    public int CountdownTime;
+    public Text display;
+    public PlantinhaGameManager manager;
+    public GameObject camTPP;
+    public AudioSource SfxTpp;
+
+
+
+    private void Start()
+    {
+        
+        StartCoroutine(Countdown());
+
+    }
+
+    IEnumerator Countdown() {
+
+        //yield return new WaitForSeconds(0.8f);
+        SfxTpp.Play();
+        yield return new WaitForSeconds(5f);
+        camTPP.SetActive(true);
+        while (CountdownTime > 0) {
+
+            display.text = CountdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            CountdownTime--;
+
+        }
+        display.text = "GO!TAKE IT HOME";
+
+        yield return new WaitForSeconds(1f);
+
+        display.gameObject.SetActive(false);
+        manager.GameStart();
+    }
+}
